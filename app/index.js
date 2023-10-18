2100a0c8e7d237dce2b8f57f71e50ff576b61759
Malek <absimalekmootez@gmail.com>
2023-10-18 12:53:53 +0200

    initilaiser le projet 11

--- app/index.js
+++ app/index.js
@@ -1,12 +1,17 @@
-const express = require('express')
+/** craation deu serveur */
+
+const express = require('express') /** importation des modules d'express */
 const cors = require('cors')
 
 const router = require('./routes')
 
-const app = express()
+const app = express() /** création d'une instance du serveur */
 app.use(cors())
-const port = 3000
+const port = 3000 /***  création du port*/
 
 app.use(router)
 
-app.listen(port, () => console.log(`Magic happens on port ${port}`))
+app.listen(port,err =>{
+    err? console.log(err) : console.log(`server is runnine on port ${port}`)
+})
+/*app.listen(port, () => console.log(`Magic happens on port ${port}`))*/
